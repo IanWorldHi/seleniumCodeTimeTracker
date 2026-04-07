d42d034e02f16325e5fd4a7305d725240ae7e448
IanWorldHi <ianchensmiley@gmail.com>
2026-04-07 11:12:56 -0400

    step1

--- uwDailyBulletin/v1.py
+++ uwDailyBulletin/v1.py
@@ -2,14 +2,21 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+#Error is because Pylance's type stubs for Selenium is outdated (confused typechcker)
+#Doesn't recognize webdriver has callable attribute - works at runtime
 driver = webdriver.Chrome()
 driver.get("https://uwaterloo.ca/daily-bulletin/")
 
-title = driver.title
-
 driver.implicitly_wait(0.5)
 
-print(title)
+linkToLatestPost = driver.find_element(By.CLASS_NAME, "card__title")
+linkToLatestPost2 = linkToLatestPost.find_element(By.TAG_NAME, "a")
+
+#For later backend use - date of latest post so only run for new posts
+datenow = linkToLatestPost2.text
+
+linkToLatestPost3 = linkToLatestPost2.get_attribute("href")
+linkToLatestPost2.click()
 
 
 
