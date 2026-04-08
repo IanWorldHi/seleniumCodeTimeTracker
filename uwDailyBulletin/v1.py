9aaa38e65c7d626eabb99f9520393c119c61f226
IanWorldHi <ianchensmiley@gmail.com>
2026-04-08 14:53:03 -0400

    dunno how to do options

--- uwDailyBulletin/v1.py
+++ uwDailyBulletin/v1.py
@@ -18,8 +18,17 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 #Allows waiting in certain scenarios vs implicitly_wait wait runs after every find element i think
 
+
+#not using rn
+opts = Options()
+opts.add_argument("--headless=new")
+
+
 #Error is because Pylance's type stubs for Selenium is outdated (confused typechcker)
 #Doesn't recognize webdriver has callable attribute - works at runtime
+
+#driver = webdriver.Chrome(options=opts) 
+#^breaks the click()
 driver = webdriver.Chrome()
 driver.get("https://uwaterloo.ca/daily-bulletin/")
 
@@ -39,13 +48,18 @@ class subTopic:
     title: str
     paragraphs: list[str] = field(default_factory=list)
     #basically calling list constructor to make empty list for each instance of subTopic - if i did = [] would be same list for all instances
-    
-    
+    images: list[dict] = field(default_factory=list) #{src, alt}
+    links: list[dict] = field(default_factory=list) #{text, href}
+    subtopics: list[dict] = field(default_factory=list) #{uhhh we'll see}
     
 
-subHeadings = driver.find_elements(By.CLASS_NAME, "uw-copy-text__wrapper ")
-#for sub in subHeadings:
-#    print(sub.get_attribute("outerHTML"))
+#def textCollector - 
+
+
+#"." instead of spaces for css selector to indicate compound classes
+subHeadings = driver.find_elements(By.CSS_SELECTOR, ".uw-text-align--left.block.block-layout-builder.block-inline-blockuw-cbl-copy-text")
+for sub in subHeadings:
+    print(sub.get_attribute("outerHTML"))
 #print(subHeadings.get_attribute("outerHTML"))
 
 #html = driver.page_source
