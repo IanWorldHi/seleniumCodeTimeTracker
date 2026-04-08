097b37c62e4949c2cb2e3eab00033522d49bc216
IanWorldHi <ianchensmiley@gmail.com>
2026-04-08 08:55:21 -0400

    did I not add the html file??

--- uwDailyBulletin/v1.py
+++ uwDailyBulletin/v1.py
@@ -1,6 +1,22 @@
+import json
 import time
+import argparse
+#Built in python module for running command line args
+
+from datetime import datetime
+from typing import Optional
+
+from dataclasses import dataclass, field, asdict
+#Let's declaring dataclasses by just writing fields so no need for init func
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+#Allows configuring how chrome launches like invisible no gui
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+#Allows waiting in certain scenarios vs implicitly_wait wait runs after every find element i think
 
 #Error is because Pylance's type stubs for Selenium is outdated (confused typechcker)
 #Doesn't recognize webdriver has callable attribute - works at runtime
@@ -18,11 +34,19 @@ datenow = linkToLatestPost2.text
 #print(linkToLatestPost2.get_attribute("outerHTML"))
 linkToLatestPost2.click()
 
+class subTopic:
+    id: str
+    title: str
+    
 
 subHeadings = driver.find_elements(By.CLASS_NAME, "uw-copy-text__wrapper ")
-for sub in subHeadings:
-    print(sub.get_attribute("outerHTML"))
+#for sub in subHeadings:
+#    print(sub.get_attribute("outerHTML"))
 #print(subHeadings.get_attribute("outerHTML"))
+
+html = driver.page_source
+with open("htmlOrg.html", "w", encoding="utf-8") as f:
+    f.write(html)
 
 
 
